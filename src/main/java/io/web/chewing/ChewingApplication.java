537fa552751787853024fa7a16bcdda8f95c068f
Crow <oneso123456789@gmail.com>
2022-11-25 15:37:33 +0900

    프로젝트 생성 및 패키지 생성(test 파일로)

---
+++ src/main/java/io/web/chewing/ChewingApplication.java
@@ -0,0 +1,13 @@
+package io.web.chewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChewingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChewingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/web/chewing/ChewingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.web.chewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChewingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
