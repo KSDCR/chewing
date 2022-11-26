c733ecaffd9a0013697b10e925ce66e0c388a52e
Crow <oneso123456789@gmail.com>
2022-11-26 17:42:57 +0900

    logfile 이름수정 및 ignore & bulld.gradle 파일에 설정없는 의존성 시큐리티 주석처리

--- src/main/java/io/web/chewing/controller/SampleController.java
+++ src/main/java/io/web/chewing/controller/SampleController.java
@@ -1,4 +1,23 @@
 package io.web.chewing.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
 public class SampleController {
+
+
+    @RequestMapping("/met01")
+    public void Met01(){
+    log.info("테스트 로그");// log.info or log.debug or log.error 등 사용 sout 사용 X
+
+
+    }
+
 }
