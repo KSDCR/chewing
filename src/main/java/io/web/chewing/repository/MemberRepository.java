4ad8298673cca0e1db4df50395cc74a2dbc1b5cd
Crow <oneso123456789@gmail.com>
2022-11-30 17:11:58 +0900

    MemberRepository에 QueryDSL로 검색 쿼리 구현

--- src/main/java/io/web/chewing/repository/MemberRepository.java
+++ src/main/java/io/web/chewing/repository/MemberRepository.java
@@ -1,7 +1,15 @@
 package io.web.chewing.repository;
 
 import io.web.chewing.Entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Object> {
+
+    @EntityGraph(attributePaths = "roleSet")
+    @Query("select m from Member m where m.email = :email")
+    Optional<Member> getWithRoles(String email);
 }
