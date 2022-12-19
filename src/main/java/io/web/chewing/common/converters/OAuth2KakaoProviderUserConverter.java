f678a179469836f4a27d420db1144ce5f0a6c237
Crow <oneso123456789@gmail.com>
2022-12-20 01:42:08 +0900

    OAuth2 kakao scope에 openid 추가시 로그인 안됨

--- src/main/java/io/web/chewing/common/converters/OAuth2KakaoProviderUserConverter.java
+++ src/main/java/io/web/chewing/common/converters/OAuth2KakaoProviderUserConverter.java
@@ -5,6 +5,7 @@ import io.web.chewing.model.ProviderUser;
 import io.web.chewing.model.social.KakaoUser;
 import io.web.chewing.common.util.OAuth2Utils;
 
+// 해당 컨버터를 DelegatingProviderUserConverter에서 List로 모아서 사용하지 않고 있었음
 public class OAuth2KakaoProviderUserConverter implements ProviderUserConverter<ProviderUserRequest, ProviderUser> {
     @Override
     public ProviderUser converter(ProviderUserRequest providerUserRequest) {
