e73890f0c8bcaffa2064d503170c075fd71cac71
Crow <oneso123456789@gmail.com>
2022-12-20 21:27:38 +0900

    2022 12 20 오후 9시 27분

--- src/main/java/io/web/chewing/config/OAuth2ClientConfig.java
+++ src/main/java/io/web/chewing/config/OAuth2ClientConfig.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Slf4j
 @Configuration
@@ -25,26 +27,24 @@ public class OAuth2ClientConfig {
     @Autowired
     CustomOidcUserService customOidcUserService;
 
-/*    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return (web) -> web.ignoring().antMatchers("/static/js/**","/static/images/**","/static/css/**","/static/scss/**");
-    }*/
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(authRequest -> authRequest
                 .antMatchers("/api/user").access("hasAnyRole('SCOPE_profile','SCOPE_profile_image', 'SCOPE_email','ROLE_USER')")
                 .antMatchers("/api/oidc").access("hasRole('SCOPE_openid')")
-                .antMatchers("/").permitAll().anyRequest().authenticated());
+                .antMatchers("/", "/resource/**").permitAll().anyRequest().authenticated());
+
 
         http.csrf().disable();
+
+        http.formLogin().loginPage("/login").loginProcessingUrl("/loginProc").defaultSuccessUrl("/").permitAll();
         http.oauth2Login(oauth2 -> oauth2
                 .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                         .userService(customOAuth2UserService)
                         .oidcUserService(customOidcUserService)));
 
         http.logout().logoutSuccessUrl("/login");
-        http.formLogin().successForwardUrl("/");
+        http.exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
         return http.build();
     }
 
--- src/main/java/io/web/chewing/controller/LoginController.java
+++ src/main/java/io/web/chewing/controller/LoginController.java
@@ -1,13 +1,26 @@
 package io.web.chewing.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 public class LoginController {
 
-    @GetMapping("/loginPage")
-    public String loginPage(){
-        return "loginPage";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, authentication);
+
+        return "redirect:/login";
     }
 }
