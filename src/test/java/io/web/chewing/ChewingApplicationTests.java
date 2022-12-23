d1e897f6b42326d9a87cd9bfe2a9a986b0bfd3fa
Crow <oneso123456789@gmail.com>
2022-12-23 17:10:32 +0900

    빌드 수정 위해서 테스트 코드 내용 삭제

--- src/test/java/io/web/chewing/ChewingApplicationTests.java
+++ src/test/java/io/web/chewing/ChewingApplicationTests.java
@@ -1,15 +1,5 @@
 package io.web.chewing;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootTest
-@EnableJpaAuditing
 class ChewingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
--- src/test/java/io/web/chewing/store/StoreTest.java
+++ src/test/java/io/web/chewing/store/StoreTest.java
@@ -1,34 +1,4 @@
-/*
 package io.web.chewing.store;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.stream.IntStream;
-
-@SpringBootTest
-@Slf4j
 public class StoreTest {
-
-    @Autowired
-    private StoreRepository storeRepository;
-
-    @Test
-    public void StoreInsert(){
-        IntStream.rangeClosed(1,5).forEach(i -> {
-            Store store = Store.builder()
-                    .name("TestStore"+i)
-                    .address("TestStore"+i)
-                    .file("TestStore"+i)
-                    .close_time("TestStore"+i)
-                    .open_time("TestStore"+i)
-                    .detail("TestStore"+i)
-                    .phone("TestStore"+i)
-            .build();
-            storeRepository.save(store);
-        } );
-    }
 }
-*/
